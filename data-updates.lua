c2da2d9087f44d4cff70ab89ef47b8da7701d9b5
Daniel Schipper <daniel_schipper_1989@hotmail.com>
2024-10-24 16:27:00 +0200

    Add settings

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,19 @@
-data.raw.tile['ice-platform'].thawed_variant = 'brash-ice'
-data.raw.tile['ice-smooth'].thawed_variant = 'brash-ice'
+local tile_collision_masks = require("__base__/prototypes/tile/tile-collision-masks")
+
+-- Smooth ice
+if settings.startup["ice-actually-melts--smooth-ice-melts"].value then
+	data.raw.tile['ice-smooth'].thawed_variant = 'brash-ice'
+end
+
+if settings.startup["ice-actually-melts--smooth-ice-buildable"].value then
+	data.raw.tile['ice-smooth'].collision_mask = tile_collision_masks.ground
+end
+
+-- Ice platform
+if settings.startup["ice-actually-melts--ice-platform-melts"].value then
+	data.raw.tile['ice-platform'].thawed_variant = 'brash-ice'
+end
+
+if settings.startup["ice-actually-melts--ice-platform-buildable"].value then
+	data.raw.tile['ice-platform'].collision_mask = tile_collision_masks.ground
+end
---
+++ settings.lua
@@ -0,0 +1,30 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "ice-actually-melts--smooth-ice-melts",
+        setting_type = "startup",
+        default_value = true,
+        order = "a-a"
+    },
+    {
+        type = "bool-setting",
+        name = "ice-actually-melts--smooth-ice-buildable",
+        setting_type = "startup",
+        default_value = true,
+        order = "a-b"
+    },
+    {
+        type = "bool-setting",
+        name = "ice-actually-melts--ice-platform-melts",
+        setting_type = "startup",
+        default_value = true,
+        order = "b-a"
+    },
+    {
+        type = "bool-setting",
+        name = "ice-actually-melts--ice-platform-buildable",
+        setting_type = "startup",
+        default_value = true,
+        order = "b-b"
+    },
+})
